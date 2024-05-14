b8b3c8b43d2bc8c29be657f4861069b2550a08f7
tdeu <thomasderouck@hotmail.com>
2024-05-14 22:35:55 +0000

    version 1.0

---
+++ src/components/Main.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+function Main() {
+  const { t } = useTranslation();
+  return (
+    <main>
+      <section className="scSiteWB parallax">
+        <div className="container" id="web">
+          <div className="scSite ">
+            <div className="scSite__1 wow fadeInLeft">
+              <img src="videos/matrix-rduite-ezgif.com-optimize.gif" alt=""/>
+            </div>
+
+            <div className="scSite__2 wow fadeInRight">
+              <h1>{t('web')}</h1>
+
+              <div className="row justify-content-center">
+                <div className="col-md-5 col-lg-4 web-col-1">
+                  <p>
+                    {" "}
+                    {t('web_p1')}
+                  </p>
+                  <p>
+                  {t('web_p2')}
+                  </p>
+                </div>
+
+                <div className="col-md-7 col-lg-8">
+                  <img
+                    src="videos/WEBSITE_animation-ezgif.com-crop.gif"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="scSiteSD parallax">
+        <div className="container" id="software">
+          <div className="scSite ">
+            <div className="scSite__2 wow fadeInLeft">
+              <h1>{t('software')}</h1>
+
+              <div className="row justify-content-center ">
+                <div className="col-md-7 col-lg-8">
+                  <img
+                    src="videos/video-software-rduite-ezgif.com-crop.gif"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-md-5 col-lg-4">
+                  <p>
+                    {t('soft_p1')}
+                  </p>
+                  <p>
+                    {t('soft_p2')}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="scSite__1 wow fadeInRight">
+              <img src="videos/matrix-rduite-ezgif.com-optimize.gif" alt=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+     
+
+      <section className="scSiteBP parallax">
+        <div className="container" id="branding">
+          <div className="scSite">
+            <div className="scSite__1">
+              <img src="videos/matrix-rduite-ezgif.com-optimize.gif" alt=""/>
+            </div>
+
+            <div className="scSite__2 wow fadeInRight">
+              <h1>{t('branding')}</h1>
+
+              <div className="row justify-content-center">
+                <div className="col-md-5 col-lg-4 brand-col-1">
+                  <p>
+                    {t('branding_p1')}
+                  </p>
+                  <p>
+                  {t('branding_p2')}
+                  </p>
+                </div>
+
+                <div className="col-md-7 col-lg-8">
+                  <img
+                    src="videos/vido-branding-rduite-ezgif.com-crop.gif"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="scSiteBrand parallax">
+        <div className="container" id="social">
+          <div className="scSite">
+            <div className="scSite__2 wow fadeInLeft">
+              <h1>{t('social')}</h1>
+
+              <div className="row justify-content-center  ">
+                <div className="col-md-7 col-lg-7 vid-par">
+                  <img
+                    src="videos/SOCIAL_animation-ezgif.com-crop.gif"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-md-5 col-lg-4 sect-par">
+                  <p>
+                    {t('social_p1')}
+                  </p>
+                  <p>
+                   {t('social_p2')}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="scSite__1 wow fadeInRight">
+              <img src="videos/matrix-rduite-ezgif.com-optimize.gif" alt=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="lifecycle">
+        <div className="container">
+          <div className="lifecycle wow fadeInLeft">
+            <h1>{t('project_life_cycle')}</h1>
+            <div className="lifecycle-img-container">
+              <img src="images/LIFECYCLE.png" alt="img" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,104 @@
+import { changeLanguage } from 'i18next';
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+import i18next from "i18next";
+
+function Nav() {
+
+    const { t } = useTranslation();
+
+    
+  return (
+  
+         <nav className="navigation">
+        <div className="container-fluid">
+            <div className="logo">
+                <a href="/">
+                    <img src="icons/LOGO WHITE.png" alt="logo" />
+                </a>
+            </div>
+
+     
+
+            <div className="menu" id="menu">
+                <ul>
+                    <li className="menu--exit">
+                        <i className="fa fa-times"></i>
+                    </li>
+
+                    <li>
+                       
+                        <span className="se"> {t('services')} <i className="fas fa-chevron-down"></i></span>
+
+                        <ul className="sub-menu ">
+                           
+
+                            <a href="#web">
+                                <li> {t('web')}</li>
+                            </a>
+
+                            <a href="#software">
+                                <li> {t('software')}</li>
+                            </a>
+
+                          
+
+
+                            <a href="#branding">
+                                <li> {t('branding')}</li>
+                            </a>
+
+                            <a href="#social" >
+                                <li> {t('social')}</li>
+                            </a>
+
+
+                            
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#contact-form" className="t">  {t('start_building')} </a>
+                    </li>
+
+                    <li type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        
+                        <a href="#about"> {t('about_us')}</a>
+                    </li>
+
+                    <li>
+                        <div className="li-lang">
+                          <span className="sreal">{i18next.language === "en" ? "EN" : "FR"}  <span style={{color:"white"}} className="sp-lang"> <i className='fas fa-chevron-down'></i></span></span> 
+                            <div className="lang">
+                                <ul>
+                                  <a href="#fr" onClick={() => {
+                                      localStorage.setItem('locales', "fr");
+                                      changeLanguage("fr");
+                                  } }>
+                                    <li>FR</li>
+                                  </a>
+                                  <a href="#en" onClick={() =>{
+                                      localStorage.setItem('locales', "en");
+                                      changeLanguage("en");
+                                  } }>
+                                    <li>EN</li>
+                                  </a>
+                                </ul>
+                              </div>
+                        </div>
+                       
+                    </li>
+                </ul>
+            </div>
+
+            
+            <div className="menu--enter" id="menu--enter">
+                <i className="fa fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+  )
+}
+
+export default Nav
